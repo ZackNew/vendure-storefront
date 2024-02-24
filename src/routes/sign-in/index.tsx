0ff26f2a77e3132216b03876abb9792a5cc416d5
ZackNew <zekariyasmuluget91@gmail.com>
2024-02-24 19:17:50 +0000

    forgot removed, reset removed, register removed

--- src/routes/sign-in/index.tsx
+++ src/routes/sign-in/index.tsx
@@ -22,12 +22,12 @@ export default component$(() => {
 		<div class="flex flex-col justify-center py-12 sm:px-6 lg:px-8">
 			<div class="sm:mx-auto sm:w-full sm:max-w-md">
 				<h2 class="mt-6 text-center text-3xl text-gray-900">Sign in to your account</h2>
-				<p class="mt-2 text-center text-sm text-gray-600">
+				{/* <p class="mt-2 text-center text-sm text-gray-600">
 					Or{' '}
 					<Link href="/sign-up" class="font-medium text-primary-600 hover:text-primary-500">
 						register a new account
 					</Link>
-				</p>
+				</p> */}
 			</div>
 
 			<div class="mt-8 sm:mx-auto sm:w-full sm:max-w-md">
